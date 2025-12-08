4b7b7c5ce2802c85c14a7777b756425855001804
anshIA09 <Anish.Pawaskar@interactiveavenues.com>
2025-12-08 16:53:04 +0530

    fix/ClipsScreen: add safe area to fix content which is going out of screen

--- src/app/clips.tsx
+++ src/app/clips.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Clips = () => {
   return (
-    <View>
-      <Text>Clips</Text>
-    </View>
+    <SafeAreaView className="flex-1" edges={["top"]}>
+      <View className="flex-1 bg-yellow-500 items-center justify-center">
+        <Text>Clips</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
