74d608d55082132ca2569fba52461ccedaf56f2b
anshIA09 <Anish.Pawaskar@interactiveavenues.com>
2025-12-08 16:55:34 +0530

    fix/ClipsScreen: fix safe area background

--- src/app/clips.tsx
+++ src/app/clips.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 const Clips = () => {
   return (
-    <SafeAreaView className="flex-1" edges={["top"]}>
+    <SafeAreaView className="flex-1 bg-yellow-500" edges={["top"]}>
       <View className="flex-1 bg-yellow-500 items-center justify-center">
         <Text>Clips</Text>
       </View>
